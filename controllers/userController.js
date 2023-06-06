21524047baa096506776389d03585711d0c5bdce
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2023-06-05 22:19:21 -0400

    Work on structure of game file

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,8 @@ const gameCanvas = document.getElementById('gameCanvas');
 const ctx = gameCanvas.getContext('2d');
 var requestID;
 
-var players = {};
+var players = [];
+var obstacles = [];
 
 var p1x = 20;
 var p1y = 200;
@@ -29,3 +30,38 @@ var spikeHurtboxXMax = 35;
 var spikeHurtboxYMin = 0;
 var spikeHurtboxYMax = 40;
 
+var clear = (e) => {
+    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+};
+
+var makeSolidBlock = (width, height, x, y) => {
+
+}
+
+// Probably make spike blocks just squares with a different color, writing code for collision detection with a triangle shape is extra work and probably not worth it unless I have time
+var makeSpikeBlock = (width, height, x, y) => {
+
+}
+
+var startGame = (e) => {
+    clear(e);
+
+    runGame();
+    obstacles.push(makeSolidBlock(50, 20));
+}
+
+var runGame = (e) => {
+
+}
+
+document.onkeydown = function (e) {
+    if (e.keyCode == 49) {
+        onFloor = false;
+        isJumping = true;
+        isFalling = false;
+        setTimeout(function () {
+            isJumping = false;
+            isFalling = true;
+        }, 1000);
+    }
+}
